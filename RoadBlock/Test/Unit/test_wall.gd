b15efdb751a21244c1be623f76b1535e44d659cc
Anthony Jhon <el_lejendario_guerrero@hotmail.com>
2019-06-08 18:20:22 -0500

    Comente el test wall

--- RoadBlock/Test/Unit/test_wall.gd
+++ RoadBlock/Test/Unit/test_wall.gd
@@ -1,27 +1,27 @@
 extends 'res://addons/gut/test.gd'
 
-var Wall=load('res://Script/Wall/wall.gd')
-var Ball=load('res://Script/Ball/ball.gd')
+var Wall=load('res://Script/Wall/wall.gd')		#Aqui referencio a la clase wall
+var Ball=load('res://Script/Ball/ball.gd')		#Aqui referencio a la clase Ball
 
-var WallObj=load('res://Perphaps/Wall/wall.tscn')
-var BallObj=load('res://Perphaps/Ball/ball.tscn')
+var WallObj=load('res://Perphaps/Wall/wall.tscn')	#Aqui creo el perprhaps
+var BallObj=load('res://Perphaps/Ball/ball.tscn')	#Aqui creo el perprahs Ball
 
-func test_can_wall():
+func test_can_wall():				#este test es para ver si tengo una clase Ball
 	var p= Wall.new()
 	assert_not_null(p)
 	
-func test_get_set_wall():
+func test_get_set_wall():			#Aqui purebo si el objeto con atributo Speed tiene get y set
 	var p= Wall.new()
 	assert_accessors(p,"speed",0,100)
 	
-func test_moves_ball():
+func test_moves_ball():				#Aqui pruebo si la bola se mueve
 	var b=Ball.new()
 	b.set_speed(10)
 	b.set_direction(Vector2(1,0))
 	simulate(b,1,1)
 	assert_eq(b.get_position(),Vector2(10,0))
 	
-func test_ball_controlar_teclas():
+func test_ball_controlar_teclas():	#Aqui pruebo si la bola no reacciona cunado presiono las teclas
 	var ball= BallObj.instance()
 	add_child(ball)
 	ball.set_position(Vector2(150,300))
@@ -33,7 +33,7 @@ func test_ball_controlar_teclas():
 	
 	remove_child(ball)
 
-func test_is_colision_ball_wall():
+func test_is_colision_ball_wall():	#Aqui pruebo si la bola se detiene cuando colisiona
 	var wall=WallObj.instance()
 	add_child(wall)
 	wall.set_position(Vector2(100,100))
@@ -52,7 +52,7 @@ func test_is_colision_ball_wall():
 	remove_child(ball)
 	remove_child(wall)
 	
-func test_controlar_collider_pared_detener():
+func test_controlar_collider_pared_detener():	#Aqui pruebo si la bola no avanza cuando hay una pared
 	var wall=WallObj.instance()
 	add_child(wall)
 	wall.set_position(Vector2(100,100))

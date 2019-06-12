1e4078b83475be6a6c711ec73729f37f302c60e2
EDE0812 <hilton.ede97@gmail.com>
2019-06-12 11:30:35 -0500

    Aqui avanzare lo de test_live

--- RoadBlock/Script/Portal/portal.gd
+++ RoadBlock/Script/Portal/portal.gd
@@ -6,6 +6,6 @@ var change=1
 
 func _on_Portal_area_entered(area):
 	colision=true
-	change=get_tree().change_scene('res://Scenas/Levels/Nivel2.tscn')
+	
 	
 
---
+++ RoadBlock/Test/Unit/test_live.gd
@@ -0,0 +1,7 @@
+extends 'res://addons/gut/test.gd'
+
+var Wall=load('res://Script/Wall/wall.gd')	#Referencio a la clase Wall
+var Ball=load('res://Script/Ball/ball.gd')	#Referencio a la clase Ball
+
+var WallObj=load('res://Perphaps/Wall/wall.tscn')	#Creo el objeto Wall
+var BallObj=load('res://Perphaps/Ball/ball.tscn')	#Creo el objeto Ball

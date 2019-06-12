95670c8440052bd320d72b3daae7a24bb5a968d0
EDE0812 <hilton.ede97@gmail.com>
2019-06-12 14:48:05 -0500

    Hice eso pensando lo de las vidas :v, si esta mal arreglar ayudenme a arreglar

--- RoadBlock/Test/Unit/test_live.gd
+++ RoadBlock/Test/Unit/test_live.gd
@@ -3,5 +3,10 @@ extends 'res://addons/gut/test.gd'
 var Wall=load('res://Script/Wall/wall.gd')	#Referencio a la clase Wall
 var Ball=load('res://Script/Ball/ball.gd')	#Referencio a la clase Ball
 
-var WallObj=load('res://Perphaps/Wall/wall.tscn')	#Creo el objeto Wall
-var BallObj=load('res://Perphaps/Ball/ball.tscn')	#Creo el objeto Ball
+var vida=load('res://Script/Live/live.gd')
+
+
+func test_live():
+	var v = vida.new()
+	assert_not_null(v)
+	

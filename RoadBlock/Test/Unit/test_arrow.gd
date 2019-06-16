55afc546f13fb39f46a1dae6cdc2132a9bd00f03
Anthony Jhon <el_lejendario_guerrero@hotmail.com>
2019-06-16 17:57:14 -0500

    Flecha corregida

--- RoadBlock/Test/Unit/test_arrow.gd
+++
@@ -1,6 +0,0 @@
-extends 'res://addons/gut/test.gd'
-# warning-ignore:unused_class_variable
-var ball=load('res://Script/Ball/ball.gd')	#Referencio a la clase Ball
-# warning-ignore:unused_class_variable
-var objball=load('res://Perphaps/Ball/ball.tscn')	#Creo el objeto Ball
-

087634fecb4c84001c16f70fd7b91a8b70d79c4b
Anthony Jhon <el_lejendario_guerrero@hotmail.com>
2019-06-22 11:43:05 -0500

    Solo me falta la animacion

--- RoadBlock/Script/Portal/portalapoyo.gd
+++ RoadBlock/Script/Portal/portalapoyo.gd
@@ -10,7 +10,14 @@ func set_portalPuente(portal):
 	_portalPuente=portal
 	
 func _on_PortalApoyo_area_entered(area):
-	colision=true
-	print ("Esto es del portal",_portalPuente)
+	if (colision==false):
+		if (_portalPuente!=null):
+			area.set_position(get_node(_portalPuente).get_position())
+			get_node(_portalPuente).colision=true
+		colision=true
+
+func _on_PortalApoyo_area_exited(area):
 	if (_portalPuente!=null):
-		area.set_position(_portalPuente.get_node("PortalApoyo").get_position())
+		if (get_node(_portalPuente).colision==true):
+			colision=false
+			get_node(_portalPuente).colision=false
--- RoadBlock/Test/Unit/test_portalapoyo.gd
+++ RoadBlock/Test/Unit/test_portalapoyo.gd
@@ -4,6 +4,7 @@ var Portal=load('res://Script/Portal/portalapoyo.gd')	#Referencio a la clase Por
 
 var BallObj=load('res://Perphaps/Ball/ball.tscn')	#Creo el objeto Ball
 var PortalObj=load('res://Perphaps/Portal/PortalApoyo.tscn')	#Creo el objeto Portal
+var WallObj=load('res://Perphaps/Wall/wall.tscn')	#Creo el objeto Wall
 
 func test_can_portal_class():				#Esto es para ver si se creo el objeto
 	var p= Portal.new()
@@ -36,7 +37,6 @@ func test_traslate_position_ball():				#verificar que la pelota colisione
 	add_child(pObj)
 	pObj.set_position(Vector2(150,100))
 	pObj.set_scale(Vector2(2,2))
-	print(pObj.get_path(),"sadasdasdasdasasdsa")
 	
 	var pObj2= PortalObj.instance()
 	add_child(pObj2)
@@ -52,7 +52,44 @@ func test_traslate_position_ball():				#verificar que la pelota colisione
 	
 	yield(yield_for(5),YIELD)
 	
-	assert_eq(pObj.colision,true)
+	assert_eq(pObj.position.x,bObj.position.x)
 	
 	remove_child(pObj)
+	remove_child(pObj2)
 	remove_child(bObj)
+	
+func test_retry_trasport():				#verificar que la pelota colisione
+	var pObj= PortalObj.instance()
+	add_child(pObj)
+	pObj.set_position(Vector2(150,100))
+	pObj.set_scale(Vector2(2,2))
+	
+	var pObj2= PortalObj.instance()
+	add_child(pObj2)
+	pObj2.set_position(Vector2(400,350))
+	pObj2.set_scale(Vector2(2,2))
+	
+	pObj.set_portalPuente(pObj2.get_path())
+	pObj2.set_portalPuente(pObj.get_path())
+	
+	var wObj= WallObj.instance()
+	add_child(wObj)
+	wObj.set_position(Vector2(400,100))
+	
+	var wObj2= WallObj.instance()
+	add_child(wObj2)
+	wObj2.set_position(Vector2(150,500))
+	
+	var bObj=BallObj.instance()
+	add_child(bObj)
+	bObj.set_position(Vector2(150,300))
+	
+	yield(yield_for(10),YIELD)
+	
+	assert_eq(pObj.position.x,bObj.position.x)
+	
+	remove_child(pObj)
+	remove_child(pObj2)
+	remove_child(bObj)
+	remove_child(wObj)
+	remove_child(wObj2)
